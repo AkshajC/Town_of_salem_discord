201f25027489a606a454c0f7531ec6193c3581a0
SuperSak <72366332+SuperSak@users.noreply.github.com>
2021-02-16 18:51:16 -0800

    I added death messages for all the roles I can remember in classic
    I added ambusher(added in a previous one), ww, jester, bodyguard, vigi, veteran, jailor, sk, arso, and maybe others that i forgot about. Some of them might be totally incorrect such as bg, ww, arso, jester, jailor, veteran, and ambusher which is most of them lmao. You can use or discard this and/or any of the roles i did incorrectly.

--- game_essentials.py
+++ game_essentials.py
@@ -18,6 +18,127 @@ class Game:
             for elem in self.players:
                 if elem[2].role.__class__.__name__ == "Janitor" and elem[2].role.is_clean is True and elem[2].role.target is target:
                     message = f"{target.name} was cleaned."
+                    
+    def kill(self, target, killer):
+        if target.role.defense < killer.role.attack:
+            if killer.role.__class__.__name__ == "Veteran":
+                message = f"{target.name} was killed by the Veteran."
+                recipient = "everyone"
+                self.dead_players.append(target)
+                del self.players[self.players.index(target)]
+            for elem in self.players:
+                if elem[2].role.__class__.__name__ == "Janitor" and elem[2].role.is_clean is True and elem[2].role.target is target:
+                    message = f"{target.name} was cleaned."
+                    
+    def kill(self, target, killer):
+        if target.role.defense < killer.role.attack:
+            if killer.role.__class__.__name__ == "Vigilante":
+                message = f"{target.name} was shot by a Vigilante."
+                recipient = "everyone"
+                self.dead_players.append(target)
+                del self.players[self.players.index(target)]
+            for elem in self.players:
+                if elem[2].role.__class__.__name__ == "Janitor" and elem[2].role.is_clean is True and elem[2].role.target is target:
+                    message = f"{target.name} was cleaned."
+                    
+    def kill(self, target, killer):
+        if target.role.defense < killer.role.attack:
+            if killer.role.__class__.__name__ == "Vampire Hunter":
+                message = f"{target.name} was staked by Vampire Hunter."
+                recipient = "everyone"
+                self.dead_players.append(target)
+                del self.players[self.players.index(target)]
+            for elem in self.players:
+                if elem[2].role.__class__.__name__ == "Janitor" and elem[2].role.is_clean is True and elem[2].role.target is target:
+                    message = f"{target.name} was cleaned."
+                    
+    def kill(self, target, killer):
+        if target.role.defense < killer.role.attack:
+            if killer.role.__class__.__name__ == "Arsonist":
+                message = f"{target.name} was incinerated by an Arsonist."
+                recipient = "everyone"
+                self.dead_players.append(target)
+                del self.players[self.players.index(target)]
+            for elem in self.players:
+                if elem[2].role.__class__.__name__ == "Janitor" and elem[2].role.is_clean is True and elem[2].role.target is target:
+                    message = f"{target.name} was cleaned."
+                    
+    def kill(self, target, killer):
+        if target.role.defense < killer.role.attack:
+            if killer.role.__class__.__name__ == "Werewolf":
+                message = f"{target.name} was mauled by the Werewolf."
+                recipient = "everyone"
+                self.dead_players.append(target)
+                del self.players[self.players.index(target)]
+            for elem in self.players:
+                if elem[2].role.__class__.__name__ == "Janitor" and elem[2].role.is_clean is True and elem[2].role.target is target:
+                    message = f"{target.name} was cleaned."
+                    
+    def kill(self, target, killer):
+        if target.role.defense < killer.role.attack:
+            if killer.role.__class__.__name__ == "Jester":
+                message = f"{target.name} was haunted by the Jester."
+                recipient = "everyone"
+                self.dead_players.append(target)
+                del self.players[self.players.index(target)]
+            for elem in self.players:
+                if elem[2].role.__class__.__name__ == "Janitor" and elem[2].role.is_clean is True and elem[2].role.target is target:
+                    message = f"{target.name} was cleaned."
+                    
+    def kill(self, target, killer):
+        if target.role.defense < killer.role.attack:
+            if killer.role.__class__.__name__ == "Bodyguard":
+                message = f"{target.name} was killed by a Bodyguard."
+                recipient = "everyone"
+                self.dead_players.append(target)
+                del self.players[self.players.index(target)]
+            for elem in self.players:
+                if elem[2].role.__class__.__name__ == "Janitor" and elem[2].role.is_clean is True and elem[2].role.target is target:
+                    message = f"{target.name} was cleaned."
+                    
+    def kill(self, target, killer):
+        if target.role.defense < killer.role.attack:
+            if killer.role.__class__.__name__ == "Vampire":
+                message = f"{target.name} was bitten by a Vampire."
+                recipient = "everyone"
+                self.dead_players.append(target)
+                del self.players[self.players.index(target)]
+            for elem in self.players:
+                if elem[2].role.__class__.__name__ == "Janitor" and elem[2].role.is_clean is True and elem[2].role.target is target:
+                    message = f"{target.name} was cleaned."
+                    
+    def kill(self, target, killer):
+        if target.role.defense < killer.role.attack:
+            if killer.role.__class__.__name__ == "Veteran":
+                message = f"{target.name} was stabbed by a Serial Killer."
+                recipient = "everyone"
+                self.dead_players.append(target)
+                del self.players[self.players.index(target)]
+            for elem in self.players:
+                if elem[2].role.__class__.__name__ == "Janitor" and elem[2].role.is_clean is True and elem[2].role.target is target:
+                    message = f"{target.name} was cleaned."
+                    
+    def kill(self, target, killer):
+        if target.role.defense < killer.role.attack:
+            if killer.role.__class__.__name__ == "Jailor":
+                message = f"{target.name} was executed by the Jailor."
+                recipient = "everyone"
+                self.dead_players.append(target)
+                del self.players[self.players.index(target)]
+            for elem in self.players:
+                if elem[2].role.__class__.__name__ == "Janitor" and elem[2].role.is_clean is True and elem[2].role.target is target:
+                    message = f"{target.name} was cleaned."
+                    
+    def kill(self, target, killer):
+        if target.role.defense < killer.role.attack:
+            if killer.role.__class__.__name__ == "Serial Killer":
+                message = f"{target.name} was stabbed by a Serial Killer."
+                recipient = "everyone"
+                self.dead_players.append(target)
+                del self.players[self.players.index(target)]
+            for elem in self.players:
+                if elem[2].role.__class__.__name__ == "Janitor" and elem[2].role.is_clean is True and elem[2].role.target is target:
+                    message = f"{target.name} was cleaned."
 
     def kill(self, target, killer):
         if target.role.defense < killer.role.attack:
@@ -29,7 +150,6 @@ class Game:
             for elem in self.players:
                 if elem[2].role.__class__.__name__ == "Janitor" and elem[2].role.is_clean is True and elem[2].role.target is target:
                     message = f"{target.name} was cleaned."
-
 
         else:
             message = "Your target's defense is too high."
